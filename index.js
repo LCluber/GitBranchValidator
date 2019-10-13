aee8bc97ecb4e915a0024c3560350a0b37c98321
lcluber <ludovic.cluber@gmail.com>
2019-10-13 12:32:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+// Delete the 0 and 1 argument (node and script.js)
+let args = process.argv.splice(process.execArgv.length + 2);
+
+// Retrieve the first argument
+let pattern = args[0];
+
+const validator = require('./lib/validate.js');
+
+validator.test();
