6bff364bfd5b71fc762abe067ca3da44cdce1aaa
LCluber <ludovic.cluber@gmail.com>
2019-10-15 00:20:09 +0200

    added custom pattern tester

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,26 @@
 // Delete the 0 and 1 argument (node and script.js)
 let args = process.argv.splice(process.execArgv.length + 2);
 
+const SUCCESS_CODE = 0;
+const FAILED_CODE = 1;
 // Retrieve the first argument
-let customPattern = args[0];
+const customPattern = args[0];
+let customRegex;
 
+if (customPattern) {
+  console.log("\x1b[37m%s\x1b[0m", "Custom pattern :");
+  console.log("\x1b[33m%s\x1b[0m", customPattern);
+  try {
+    customRegex = new RegExp(customPattern);
+  } catch (e) {
+    console.error("\x1b[31m%s\x1b[0m", "Error: Invalid custom pattern");
+    process.exitCode = FAILED_CODE;
+    return false;
+  }
+}
 const gitBranch = require("current-git-branch");
 const validator = require("../lib/validate.js");
 
 let branchName = gitBranch();
 
-validator.test(branchName, customPattern);
+validator.test(branchName, customRegex);
